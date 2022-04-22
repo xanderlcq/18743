7b79ed518ab744c6863f8f82c5ae96c50c861d13
Xander <x@xanderli.com>
2022-04-22 08:55:48 -0400

    added mem unit

--- src/rtl/delay/tb/testbench.sv
+++ src/rtl/delay/tb/testbench.sv
@@ -121,52 +121,107 @@ module tb();
 
     `endif
 
-    `ifdef FALLING
-        // task reset();
-        //     grst = 1;
-        //     input_spikes = '1;
-        //     @(posedge aclk);
-        //     @(posedge aclk);
-        //     @(posedge aclk);
-        //     grst = 0;
-        //     @(posedge aclk);
-        //     @(posedge aclk);
-        // endtask
+    `ifdef RISING
+        task reset();
+            grst <= 1;
+            in = '0;
+            delay = '0;
+            @(posedge aclk);
+            grst <= 0;
 
-        // initial begin
-        //     $display("Testing Falling Edge of wta1");
-        //     reset();
-        //     input_spikes[0] <= 1'b0;
-        //     @(posedge aclk);
-        //     @(posedge aclk);
+        endtask
 
-        //     reset();
-        //     @(posedge aclk);
-        //     input_spikes[3] <= 1'b0;
-        //     @(posedge aclk);
-        //     input_spikes[7] <= 1'b0;
-        //     @(posedge aclk);
-        //     input_spikes[6] <= 1'b0;
-        //     @(posedge aclk);
-        //     @(posedge aclk);
-        //     input_spikes[2] <= 1'b0;
-        //     @(posedge aclk);
-        //     @(posedge aclk);
+        initial begin
+            $display("Testing Rising Edge of naive delay");
+            reset();
+            in <= 1'b1;
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
 
-        //     reset();
-        //     @(posedge aclk);
-        //     input_spikes[1] <= 1'b0;
-        //     input_spikes[4] <= 1'b0;
-        //     input_spikes[5] <= 1'b0;
-        //     @(posedge aclk);
-        //     input_spikes[6] <= 1'b0;
-        //     @(posedge aclk);
-        //     @(posedge aclk);
-        //     input_spikes[2] <= 1'b0;
-        //     @(posedge aclk);
-        //     @(posedge aclk);
-        //     $finish;
-        // end
+            reset();
+            in <= 1'b1;
+            delay <= 4'd2;
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+
+            reset();
+            in <= 1'b1;
+            delay <= 4'd5;
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+
+            reset();
+            delay <= 4'd6;
+            @(posedge aclk);
+            @(posedge aclk);
+            in <= 1'b1;
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+
+
+            reset();
+            delay <= 4'd12;
+            in <= 1'b1;
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            @(posedge aclk);
+            reset();
+
+           
+            
+
+            $finish;
+        end
 
 
     `endif
---
+++ src/rtl/mem/mem.sv
@@ -0,0 +1,70 @@
+`default_nettype none
+
+/***
+* Rising Edge Implementation
+* If the gamma cycle is G, we can delay by at most G-1 unit
+* Gamma cycle should be a power of 2
+* Assume delay remain constant during one gamma cycle
+***/
+module mem
+#(  parameter GAMMA_CYCLE_WIDTH=16,
+    parameter PULSE_WIDTH=8)
+(
+    input logic grst, //this is used to signal the start of a new gamma cycle, must be assert during first reset
+    input logic aclk,
+    input logic in,
+    output logic out,
+    input logic rst //must be asserted every time to start function correctly
+);
+
+`ifdef RISING
+    logic spiked, spiked_next;
+    logic [$clog2(GAMMA_CYCLE_WIDTH) - 1 : 0] counter, counter_next;
+
+    // always_comb begin
+    //     if(spiked) begin
+    //         spiked_next = 1'b1;
+    //     end else if(in) begin
+    //         spiked_next = 1'b1;
+    //     end else begin
+    //         spiked_next = 1'b0;
+    //     end
+    // end
+    assign spiked_next = spiked | in;
+
+    always_ff @(posedge aclk, posedge rst) begin
+        if(rst) begin
+            spiked <= '0;
+        end else begin
+            spiked <= spiked_next;
+        end
+    end
+
+    assign counter_next = (spiked_next) ? counter + 1 : counter;
+
+    always_ff @(posedge aclk, posedge rst) begin
+        if(rst) begin
+            counter <= '0;
+        end else begin
+            counter <= counter_next;
+        end
+    end
+
+    // 0 1 2 3 4 5 6 7 | 
+    // 0 0 0 ^0 1 2 3 4 | 5 6 7 ^0 1 2 3 4 | 
+    logic should_spike, should_spike_next;
+    assign should_spike_next = (counter_next == 1) | should_spike;
+
+    assign out = should_spike_next & spiked_next;
+
+    always_ff @(posedge aclk, posedge grst) begin
+        if(grst) begin
+            should_spike <= '0;
+        end else begin 
+            should_spike <= should_spike_next; 
+        end
+    end
+
+`endif 
+
+endmodule
